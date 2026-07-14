6f4bf0c0d22fd6c7ffefef89c34d692fc4b55a34
Guangzhen Jin <jin456@purdue.edu>
2026-07-14 15:40:55 -0400

    Update modules.

---
+++ modulefiles/gilbreth/external-20250225/sentaurus/2026.03.lua
@@ -0,0 +1,19 @@
+local app = "sentaurus"
+local version = "Y-2026.03"
+local modroot = pathJoin("/apps/external/synopsys", app, version)
+
+whatis("invoke " .. app .. " " .. version)
+
+local err_message="\nWARNING: Access to this software is restricted. For more information, see https://engineering.purdue.edu/ECEIL/Lab_Support/EDA/\n"
+local found = required_group("cae2")
+if (not found and mode() == "load") then
+    LmodError(err_message)
+end
+
+depends_on("libxscrnsaver")
+depends_on("mesa-glu")
+
+setenv("STROOT",modroot)
+setenv("LM_LICENSE_FILE","1718@marina.ecn.purdue.edu")
+
+prepend_path("PATH",modroot.."/bin")
